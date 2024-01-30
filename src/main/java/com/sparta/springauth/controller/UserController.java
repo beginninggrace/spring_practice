2a86a40a6f6d8d0bbf2f7135141943659e4666d1
si eun <tldms123455@naver.com>
2024-01-30 23:28:38 +0900

    3-15 Validation 예외처리

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -4,19 +4,24 @@ import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequestMapping("/api")
 public class UserController {
 
-    // UserService의 signup 메소드에 전달하기 위해 주입(DI) 받아와야 함
     private final UserService userService;
 
-    // alt + ins로 생성자 만들어서 생성자로 주입해줬음
     public UserController(UserService userService) {
         this.userService = userService;
     }
@@ -32,11 +37,18 @@ public class UserController {
     }
 
     @PostMapping("/user/signup")
-    public String signup(SignupRequestDto requestDto) {
-        userService.signup(requestDto); // 위에서 생성자로 주입 받았으면 userService signup 메소드에 받아온 request(requestDto) 전달
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if(fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/user/signup";
+        }
 
-        return "redirect:/api/user/login-page"; // 문제 없이 수행되면 회원가입이 다 완료된거니 로그인 하라는 의미로 로그인 페이지를 반환
+        userService.signup(requestDto);
+
+        return "redirect:/api/user/login-page";
     }
-
-
 }
